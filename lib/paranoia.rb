ea00843bc62cc308a32e60a0046d5130c85b9b88
jdejong <jdejong@sparknettech.com>
2011-04-22 12:49:25 -0400

    ok

--- lib/paranoia.rb
+++ lib/paranoia.rb
@@ -4,7 +4,7 @@ module Paranoia
   end
   def destroy
     _run_destroy_callbacks
-	self.update_attributes(:deleted_at => Time.now)
+	self.update_attributes!(:deleted_at => Time.now)
     #self[:deleted_at] ||= Time.now
     #self.save!    
   end
